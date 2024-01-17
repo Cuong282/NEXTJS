42f9f79d2bf66716b5d91ad79f5e8536d08936ef
cuong <ntcuong2822002@gmail.com>
2024-01-17 21:56:40 +0700

    111

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
-import AppTable from "./components/app.table"
+import useSWR from "swr";
+import AppTable from "./components/app.table";
 import { useEffect } from "react";
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export default function Home() {
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
+    });
+console.log(data)
 
-  useEffect(()=>{
-    const fetchData = async () =>{
-      const res = await fetch ("http://localhost:8000/blogs");
-      const data = await res.json();
-      console.log(data)
+    if(!data){
+      return <div>loading...</div>
     }
-    fetchData();
-  },[])
-
   return (
     <>
-    <AppTable/>
+    {/* <div>{data?.length}</div> */}
+      <AppTable
+      blogs={data}
+      />
     </>
-  
-  )
+  );
 }
